df4c0211f0f515cc28ee36ea4844def326a555f6
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2020-07-30 18:44:01 -0300

    Prisma
    implementando prisma

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import User from '../schemas/User'
+import UserRepository from '../repositories/UserRepository';
+import UserService from '../services/UserService';
+
+class UserController {
+    public async index (req: Request, res: Response): Promise<Response>  {
+        
+        //const users = await User.find()
+        const users = await UserService.findManyUsers()
+
+        return res.json(users)
+
+    }
+
+    public async store (req: Request, res: Response): Promise<Response>  {
+
+        const user = await User.create(req.body)
+
+        return res.json(user)
+    }
+}
+
+export default new UserController()
